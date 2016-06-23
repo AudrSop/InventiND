64071a3bb73d21c346b85387094eca03d0b1c816
audrone_sopauskaite <audrone.sop@gmail.com>
2016-06-23 14:54:20 +0300

    Removing accidental commit

--- playground-camel/src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
+++ playground-camel/src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
@@ -6,6 +6,5 @@ public class PlaygroundRouteBuilder extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("timer://myTimer?fixedRate=true&period=2000").to("bean:firstBean?method=helloWorldMethod");
     }
 }
