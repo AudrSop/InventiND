34f78bdc30af457a0c681631eee7dd3d2e43b278
audrone_sopauskaite <audrone.sop@gmail.com>
2016-06-26 19:01:09 +0300

    Second commit for third task. Added main functonality(text to lower case + signature) and variotion from file content

---
+++ playground-camel/src/main/java/lt/inventi/playground/LowerCaseClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lt.inventi.playground;
+
+/**
+ *
+ * @author Burbulas
+ */
+public class LowerCaseClass {
+
+    public String toLower(String s) {
+        return s.toLowerCase();
+    }
+
+}
--- playground-camel/src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
+++ playground-camel/src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
@@ -7,6 +7,12 @@ public class PlaygroundRouteBuilder extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("file://../?fileName=MyFile.txt&charset=utf-8&noop=true")
-                .to("file://../?fileName=${file:name.noext}-result.${file:ext}&charset=utf-8");
+                .choice()
+                    .when().simple("${body} != ''")
+                        .to("bean:lowerCase?method=toLower(${body})")
+                        .setBody(body().append(" My random additional Worlds!"))
+                        .to("file://../?fileName=${file:name.noext}-result.${file:ext}&charset=utf-8")
+                    .otherwise()
+                        .to("file://../?fileName=${file:name.noext}-result.${file:ext}&charset=utf-8");
     }
 }
