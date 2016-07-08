4e5fc52f38feaf21a71097343fbf1466e9577dde
AudrSop <audrone.sop@gmail.com>
2016-07-08 10:50:38 +0300

    Code for sixth task part A.

--- playground-camel/src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
+++ playground-camel/src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
@@ -3,15 +3,14 @@ package lt.inventi.playground;
 
 import org.apache.camel.builder.RouteBuilder;
 
-
+import org.apache.camel.builder.xml.Namespaces;
 
 public class PlaygroundRouteBuilder extends RouteBuilder {
-
+    Namespaces ns = new Namespaces("pin", "http://pingpong.bpel.tps");
     @Override
     public void configure() throws Exception {
-        from("cxf:bean:pingPongEndpoint")
-                .to("log:info").end();
-
-
+        from("cxf:bean:pingPongEndpoint").log("${body}").setHeader("coolHeader")
+                .xpath("/pin:echoInput/pin:input/text()", String.class, ns).to("log:info")
+                .setBody(simple("<element>" + "${header.coolHeader}" + "</element>")).to("log:info");
     }
 }
