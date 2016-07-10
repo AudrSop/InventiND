6000444bd5bc0de47ea95b9c122a14014f086f74
audrone_sopauskaite <audrone.sop@gmail.com>
2016-07-10 20:03:55 +0300

    Second part of sixth task(with jaxb)

--- playground-camel/src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
+++ playground-camel/src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
@@ -4,13 +4,18 @@ package lt.inventi.playground;
 import org.apache.camel.builder.RouteBuilder;
 
 import org.apache.camel.builder.xml.Namespaces;
+import org.apache.camel.converter.jaxb.JaxbDataFormat;
+import org.apache.camel.spi.DataFormat;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 
 public class PlaygroundRouteBuilder extends RouteBuilder {
-    Namespaces ns = new Namespaces("pin", "http://pingpong.bpel.tps");
+
     @Override
     public void configure() throws Exception {
-        from("cxf:bean:pingPongEndpoint").log("${body}").setHeader("coolHeader")
-                .xpath("/pin:echoInput/pin:input/text()", String.class, ns).to("log:info")
-                .setBody(simple("<element>" + "${header.coolHeader}" + "</element>")).to("log:info");
+        JAXBContext ctx = JAXBContext.newInstance(EchoInput.class);
+        DataFormat jaxBDataFormat = new JaxbDataFormat(ctx);
+        from("cxf:bean:pingPongEndpoint").unmarshal(jaxBDataFormat).to("log:info");
     }
 }
