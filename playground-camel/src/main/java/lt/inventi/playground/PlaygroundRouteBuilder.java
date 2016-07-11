ddfc134433a4f0cf02a89a4d96769dcfb7a2641a
AudrSop <audrone.sop@gmail.com>
2016-07-11 09:02:49 +0300

    Seventh task, needs check

--- playground-camel/src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
+++ playground-camel/src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
@@ -2,13 +2,9 @@ package lt.inventi.playground;
 
 
 import org.apache.camel.builder.RouteBuilder;
-
-import org.apache.camel.builder.xml.Namespaces;
 import org.apache.camel.converter.jaxb.JaxbDataFormat;
 import org.apache.camel.spi.DataFormat;
-
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
 
 public class PlaygroundRouteBuilder extends RouteBuilder {
 
