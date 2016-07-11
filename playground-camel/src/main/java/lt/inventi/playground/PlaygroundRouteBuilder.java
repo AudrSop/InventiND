49450fedd87c371108ef0d903aaaf23deac779d7
AudrSop <audrone.sop@gmail.com>
2016-07-11 13:00:11 +0300

    First task fixes

--- playground-camel/src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
+++ playground-camel/src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
@@ -6,6 +6,6 @@ public class PlaygroundRouteBuilder extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("timer://myTimer?fixedRate=true&period=2000").to("bean:firstBean?method=helloWorldMethod");
+        from("timer://myTimer?fixedRate=true&period=2000").bean(FirstBean.class, "helloWorldMethod");
     }
 }
