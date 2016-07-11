95027ce4d39791c722d5aa5fd59eda636f70b689
AudrSop <audrone.sop@gmail.com>
2016-07-11 15:11:05 +0300

    changed data format to "MESSAGE" and removed unmarshaling  # Please enter the commit message for your changes. Lines starting

--- playground-camel/src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
+++ playground-camel/src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
@@ -2,16 +2,11 @@ package lt.inventi.playground;
 
 
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.converter.jaxb.JaxbDataFormat;
-import org.apache.camel.spi.DataFormat;
-import javax.xml.bind.JAXBContext;
 
 public class PlaygroundRouteBuilder extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        JAXBContext ctx = JAXBContext.newInstance(EchoInput.class);
-        DataFormat jaxBDataFormat = new JaxbDataFormat(ctx);
-        from("cxf:bean:pingPongEndpoint").unmarshal(jaxBDataFormat).to("log:info").to("cxf:bean:soapEndpoint").to("log:info");
+        from("cxf:bean:pingPongEndpoint").to("log:info").to("cxf:bean:soapEndpoint").to("log:info");
     }
 }
