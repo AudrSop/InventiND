0444f9347046494e1ed15cf53c270485da493dbb
AudrSop <audrone.sop@gmail.com>
2016-07-11 14:05:15 +0300

    Fixes in 6 task A part

--- playground-camel/src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
+++ playground-camel/src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
@@ -3,14 +3,14 @@ package lt.inventi.playground;
 
 import org.apache.camel.builder.RouteBuilder;
 
-import org.apache.camel.builder.xml.Namespaces;
-
 public class PlaygroundRouteBuilder extends RouteBuilder {
-    Namespaces ns = new Namespaces("pin", "http://pingpong.bpel.tps");
     @Override
     public void configure() throws Exception {
-        from("cxf:bean:pingPongEndpoint").log("${body}").setHeader("coolHeader")
-                .xpath("/pin:echoInput/pin:input/text()", String.class, ns).to("log:info")
-                .setBody(simple("<element>" + "${header.coolHeader}" + "</element>")).to("log:info");
+        from("cxf:bean:pingPongEndpoint").log("${body}")
+                .setHeader("coolHeader", xpath("/pin:echoInput/pin:input/text()").resultType(String.class).namespace("pin", "http://pingpong.bpel.tps"))
+                .setBody(simple("<echoInputResponse xmlns=\"http://pingpong.bpel.tps\">" + "<echoInputReturn>" + "${header.coolHeader}"
+                        + "</echoInputReturn>" + "</echoInputResponse>")).to("log:info");
     }
+
+
 }
