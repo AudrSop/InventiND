c7be8f67d13261029061713e67b3b522b6f34cef
AudrSop <audrone.sop@gmail.com>
2016-07-12 08:54:44 +0300

    Tenth task code.

--- playground-camel/src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
+++ playground-camel/src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
@@ -6,10 +6,10 @@ public class PlaygroundRouteBuilder extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-         from("file://../?fileName=MyFile.txt&charset=utf-8&noop=true").split(body().tokenize("\\n"),
-                 new AggregationCounter())
+         from("file://../?fileName=MyFile.txt&charset=utf-8&noop=true")
                 .to("activemq:queue:dummy?asyncConsumer=true").log("${body} First Thread");
 
-         from("activemq:queue:dummy?requestTimeout=500").log("${body} Second Thread").to("file://../?fileName=${file:name.noext}-result.${file:ext}&charset=utf-8");
+         from("activemq:queue:dummy").delay(200).split(body().tokenize("\\n"), new AggregationCounter()).log("${body} Second Thread").end()
+                 .to("file://../?fileName=${file:name.noext}-result.${file:ext}&charset=utf-8");
     }
 }
