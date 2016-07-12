419e26489e6f931e01687d11322266e1ce54294b
AudrSop <audrone.sop@gmail.com>
2016-07-12 11:27:50 +0300

    Removing unnecessary property

--- playground-camel/src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
+++ playground-camel/src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
@@ -7,7 +7,7 @@ public class PlaygroundRouteBuilder extends RouteBuilder {
     @Override
     public void configure() throws Exception {
          from("file://../?fileName=MyFile.txt&charset=utf-8&noop=true")
-                .to("activemq:queue:dummy?asyncConsumer=true").log("${body} First Thread");
+                .to("activemq:queue:dummy").log("${body} First Thread");
 
          from("activemq:queue:dummy").delay(200).split(body().tokenize("\\n"), new AggregationCounter()).log("${body} Second Thread").end()
                  .to("file://../?fileName=${file:name.noext}-result.${file:ext}&charset=utf-8");
