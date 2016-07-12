f96ea74b03448a4ccf1a0c104fe8e54ae27489b8
AudrSop <audrone.sop@gmail.com>
2016-07-12 09:11:30 +0300

    removed static variable for line counter. Inplemented line counter with Camel Split Index

--- playground-camel/src/main/java/lt/inventi/playground/AggregationCounter.java
+++ playground-camel/src/main/java/lt/inventi/playground/AggregationCounter.java
@@ -14,23 +14,19 @@ import org.apache.camel.processor.aggregate.AggregationStrategy;
  */
 public class AggregationCounter implements AggregationStrategy {
 
-    public static int lineCounter = 1;
-
     @Override
     public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
 
         if (oldExchange == null) {
             String newBody = newExchange.getIn().getBody(String.class);
-            String body = lineCounter + ". " + newBody + "\n";
-            lineCounter++;
+            String body = (newExchange.getProperty("CamelSplitIndex", Integer.class) +1) +". " + newBody + "\n";
             newExchange.getIn().setBody(body);
             return newExchange;
         }
 
         String oldBody = oldExchange.getIn().getBody(String.class);
         String newBody = newExchange.getIn().getBody(String.class);
-        String body = oldBody + lineCounter + ". " + newBody + "\n";
-        lineCounter++;
+        String body = oldBody + (newExchange.getProperty("CamelSplitIndex", Integer.class) +1) + ". " + newBody + "\n";
         oldExchange.getIn().setBody(body);
 
         return oldExchange;
